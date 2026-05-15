133c5761ed409a89483342d0b5874aaf68c2b12c
Omar mohamed <omarshehata3232@gmail.com>
2026-05-15 15:57:45 +0300

    trying deploy with port 80 v1

--- main.py
+++ main.py
@@ -45,9 +45,7 @@ class ChurnRequest(BaseModel):
 @get("/")
 async def home() -> dict:
     logger.info("Home endpoint accessed")
-    return {
-        "message": "Welcome to the Churn Prediction API! Visit /schema/swagger for docs."
-    }
+    return {"message": "Welcome to the Churn Prediction API."}
 
 
 # TODO 3: Create a GET endpoint at "/health" that returns {"status": "healthy"}
